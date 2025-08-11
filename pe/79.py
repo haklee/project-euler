e719d49c8a9d3628c08b8814b1fbd73190ff0e72
haklee <haklee@haklee.com>
2025-08-11 17:40:43 +0900

    79 py

---
+++ pe/79.py
@@ -0,0 +1,15 @@
+import re
+
+a = sorted(list([i.replace("\n", "") for i in open(0)]))
+s = list("".join(a))
+for i in range(len(s)):
+    t = list(s)
+    t[i] = "x"
+    for p in a:
+        r = re.findall(f".*{p[0]}.*{p[1]}.*{p[2]}.*", "".join(t))
+        if not r:
+            break
+    else:
+        s = t
+
+print("".join([i for i in s if i != "x"]))
